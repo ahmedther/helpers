68a1467d8acf35292adc4f1c36179b2c84719a3c
Ahmed <aa10980@gmail.com>
2024-02-02 17:30:13 -0400

    .

--- py_res_helper/resume_cover_merge.bat
+++ py_res_helper/resume_cover_merge.bat
@@ -1,5 +1,6 @@
 @echo off
-cd "C:\Users\AHMED\Desktop\AHMED\Resume\pdf\pdf_merger\Scripts"
+cd "C:\Users\AHMED\Desktop\AHMED\Resume\py_helper\py_res_helper_venv\Scripts"
 call activate
+cd "C:\Users\AHMED\Desktop\AHMED\Resume\py_helper\py_res_helper"
 python merge_pdf.py
 echo Completed
